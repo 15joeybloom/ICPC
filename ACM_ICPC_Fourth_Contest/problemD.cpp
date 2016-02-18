cdaa4d76ddff9f60459651f77f3dbb508b166fee
Joey Bloom <jpbloom2@illinois.edu>
2016-02-18 15:52:58 -0600

    Began work on contest 4 problem D.

--- ACM_ICPC_Fourth_Contest/problemD.cpp
+++ ACM_ICPC_Fourth_Contest/problemD.cpp
@@ -9,5 +9,49 @@ using namespace std;
 
 int main()
 {
+    int A, B;
+    char Sin[5000];
+    bool S[5000];
+
+    while(scanf("%d %d",&A, &B)!=EOF)
+    {
+        scanf("%s",&Sin);
+
+        int n = 0, p = 0;
+
+        for(int i = 0; Sin[i]!='\0'; i++)
+        {
+            if(Sin[i]=='B')
+            {
+                S[i] = true;
+                p++;
+            }
+            else S[i] = false;
+
+            n++;
+        }
+
+        int minCost = 0;
+        int numToMove = 0;
+        for(int i = p; i < n; i++)
+        {
+            if(S[i]) numToMove++;
+        }
+        minCost = numToMove*A;
+
+        int curr = p;
+        for(int i = 1; i <= numToMove; i++)
+        {
+            while(!S[curr]) curr++;
+            
+            int cost = (numToMove-i)*A;
+            for(int j = curr; j >= p; j--)
+            {
+
+            }
+        }
+
+        
+    }
 
 }
