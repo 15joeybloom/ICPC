6aa8b5fd10a37b7ca82e92534b6a2faf66e702ea
Joey Bloom <15joeybloom@gmail.com>
2016-03-09 01:44:27 -0600

    Finished ACM_ICPC_Seventh_Contest

--- ACM_ICPC_Seventh_Contest/problemB.cpp
+++ ACM_ICPC_Seventh_Contest/problemB.cpp
@@ -5,46 +5,69 @@
 #include <climits>
 #include <vector>
 #include <queue>
+#include <list>
 #include <cstring>
 using namespace std;
 
+int xs[8] = { -1, -1, -1, 0, 1, 1, 1, 0};
+int ys[8] = { -1, 0, 1, 1, 1, 0, -1, -1};
+
 char grid[52][52];
+bool visited[52][52];
 
 int main()
 {
     int H, W;
     list<int> bfsx;
     list<int> bfsy;
-    while(scanf("%d %d", &H, &W)!=EOF && H!= 0 && W!=0)
+    for(int cas = 1; scanf("%d %d", &H, &W)!=EOF && H!= 0 && W!=0; cas++)
     {
-        for(int i = 0; i < H; i++)
+        for(int i = 0; i <= H; i++)
         {
-            for(int j = 0; j < W; j++)
+            for(int j = 0; j <= W; j++)
             {
-                scanf("%c",&grid[i][j]);
-                if(grid[i][j] == 'A')
+                grid[j][i] = 0;
+                visited[j][i] = false;
+            }
+        }
+        for(int i = 1; i <= H; i++)
+        {
+            scanf("\n");
+            for(int j = 1; j <= W; j++)
+            {
+                scanf("%c",&grid[j][i]);
+                if(grid[j][i] == 'A')
                 {
-                    bfsx.push_back(i);
-                    bfsy.push_back(j);
+                    bfsx.push_back(j);
+                    bfsy.push_back(i);
                 }
             }
-            scanf("\n");
         }
 
+        char mymax = 'A'-1;
+        while(!bfsx.empty())
+        {
+            int x = bfsx.front();
+            int y = bfsy.front();
+            bfsx.pop_front();
+            bfsy.pop_front();
+
+            mymax = grid[x][y];
+
+            for(int i = 0; i < 8; i++)
+            {
+                int tempx = x+xs[i];
+                int tempy = y+ys[i];
+                if(!visited[tempx][tempy] && grid[tempx][tempy] == grid[x][y]+1)
+                {
+                    bfsx.push_back(tempx);
+                    bfsy.push_back(tempy);
+                    visited[tempx][tempy] = true;
+                }
+            }
+        }
+
+        printf("Case %d: %d\n", cas, mymax-'A'+1);
     }
-
-    char mymax = 'A'-1;
-    while(!bfsx.empty())
-    {
-        int x = bfsx.front();
-        int y = bfsy.front();
-        bfsx.pop_front();
-        bfsy.pop_front();
-
-        mymax = grid[x][y];
-
-        if(grid[x
-    }
-
 
 }
--- ACM_ICPC_Seventh_Contest/problemF.cpp
+++ ACM_ICPC_Seventh_Contest/problemF.cpp
@@ -8,7 +8,52 @@
 #include <cstring>
 using namespace std;
 
+int parent[1000000];
+
+int find(int x)
+{
+    if(x == parent[x])
+        return x;
+    return parent[x] = find(parent[x]);
+}
 int main()
 {
+    int n, m;
+    scanf("%d %d\n", &n, &m);
+    for(int r = 0; r < n; r++)
+        for(int c = 0; c < m; c++)
+            parent[r*m+c] = r*m+c;
+    for(int r = 0; r < n; r++)
+    {
+        for(int c = 0; c < m; c++)
+        {
+            int x = r*m+c;
+            char dir;
+            scanf("%c", &dir);
+            switch(dir)
+            {
+                case 'N':
+                    parent[x] = find(x-m);
+                    break;
+                case 'E':
+                    parent[x] = find(x+1);
+                    break;
+                case 'S':
+                    parent[x] = find(x+m);
+                    break;
+                case 'W':
+                    parent[x] = find(x-1);
+                    break;
+                default:
+                    printf("Joey we got a problem %d", dir);
+            }
+        }
+        scanf("\n");
+    }
 
+    int counter = 0;
+    for(int i = 0; i < n*m; i++)
+        if(parent[i] == i) counter++;
+
+    printf("%d\n", counter);
 }
