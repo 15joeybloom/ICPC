9fcc1b187d137c90666364992098baaee1b1d032
Joey Bloom <15joeybloom@gmail.com>
2016-04-19 09:54:48 -0500

    Updated template with more includes

--- template/D.cpp
+++ template/D.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <algorithm>
 #include <cmath>
 #include <climits>
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <iterator>
+#include <list>
+#include <set>
+#include <map>
+#include <bitset>
 using namespace std;
 
 int main()
