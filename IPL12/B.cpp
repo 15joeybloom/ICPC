3e99c3514b473ddaa66ff4a5ec8c58c6795d2749
Joey Bloom <15joeybloom@gmail.com>
2018-04-23 21:47:45 -0500

    Found some changes, idk what they are! haha!

--- IPL12/B.cpp
+++ IPL12/B.cpp
@@ -14,6 +14,32 @@
 #include <bitset>
 using namespace std;
 
+int a[100010];
+int partialsums[100010];
+int p[100010];
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", a+i, p+i);
+    }
+    partialsums[0] = a[0];
+    for(int i = 1; i < n; i++) {
+        partialsums[i] = a[i]+partialsums[i-1];
+    }
+
+    int lastfed = n;
+    int cost = 0;
+    for(int price = 1; price <= 100; price++) {
+        for(int i = 0; i < lastfed; i++) {
+            if(p[i] == price) {
+                cost += price*(partialsums[lastfed-1]-partialsums[i-1]);
+                lastfed = i;
+                break;
+            }
+        }
+    }
+    printf("%d\n", cost);
 }
