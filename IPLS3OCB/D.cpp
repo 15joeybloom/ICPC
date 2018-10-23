4f83b1c5a9bb7daaaa9f89a477733be3581b2f53
Joey Bloom <15joeybloom@gmail.com>
2018-10-22 22:00:33 -0500

    IPLS3OCB stuff found on desktop

--- IPLS3OCB/D.cpp
+++ IPLS3OCB/D.cpp
@@ -2,59 +2,118 @@
 #include <limits.h>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+#define MOD 1000000007
 typedef long long LL;
 
 struct Edge {
-    LL u,v,d;
+    LL to,d;
 };
 
 LL N,M,S,T,distS[100010],distT[100010],numS[100010],numT[100010];
 vector<Edge> adj[100010];
+bool visited[100010];
+
+struct cmp {
+    bool reverse;
+    LL *dist;
+    cmp(LL* d, bool r) { dist = d; reverse = r; }
+    bool operator()(LL left, LL right) {
+        return reverse != (dist[left] < dist[right]);
+    }
+};
+void dijkstra(LL *dist, LL source) {
+    for (int i = 1; i <= N; i++) visited[i] = false;
+    std::priority_queue<LL, std::vector<LL>, cmp> q(cmp(dist,true));
+    q.push(source);
+    dist[source] = 0;
+    while (!q.empty()) {
+        LL curr = q.top();
+        q.pop();
+
+        if (!visited[curr]) {
+            visited[curr] = true;
+            for (Edge f : adj[curr]) {
+                if (!visited[f.to] && dist[f.to] > dist[curr] + f.d) {
+                    dist[f.to] = dist[curr] + f.d;
+                    q.push(f.to);
+                }
+            }
+        }
+    }
+    /*
+    for (int i = 1; i <= N; i++) {
+        printf("%lld ", dist[i]);
+    }
+    printf("\n");
+    */
+}
+
+void countShortestPaths(LL *num, LL *dist, LL source) {
+    vector<LL> vertices;
+    for (int i = 1; i <= N; i++) vertices.push_back(i);
+    sort(vertices.begin(), vertices.end(), cmp(dist,false));
+    num[source] = 1;
+    for (LL v : vertices) {
+        for (Edge e : adj[v]) {
+            if (dist[e.to] == dist[v] + e.d) {
+                num[e.to] += num[v];
+                num[e.to] %= MOD;
+            }
+        }
+    }
+    /*
+    for (int i = 1; i <= N; i++) {
+        printf("%lld ", num[i]);
+    }
+    printf("\n");
+    */
+}
 
 int main() {
     for (int i = 0; i < 100010; i++) {
-        distS[i] = distT[i] = LLONG_MAX;
+        distS[i] = distT[i] = LLONG_MAX/2;
         numS[i] = numT[i] = 0;
     }
 
     scanf("%lld %lld", &N, &M);
     scanf("%lld %lld", &S, &T);
     for (int i = 0; i < M; i++) {
-        Edge e;
-        scanf("%lld %lld %lld", &e.u, &e.v, &e.d);
-        adj[e.u].push_back(e);
-        adj[e.v].push_back(e);
+        Edge e1,e2;
+        scanf("%lld %lld %lld", &e1.to, &e2.to, &e1.d);
+        e2.d = e1.d;
+        adj[e1.to].push_back(e2);
+        adj[e2.to].push_back(e1);
     }
 
+    dijkstra(distS, S);
+    dijkstra(distT, T);
+    LL D = distS[T];
 
-    auto cmp = [](Edge left, Edge right) { 
-        return left.d > right.d;
-    };
-    std::priority_queue<Edge, std::vector<Edge>, decltype(cmp)> qS(cmp);
-    for ( Edge e : adj[S] ) qS.push(e);
-    distS[S] = 0;
-    while (!qS.empty()) {
-        Edge e = qS.top();
-        qS.pop();
+    countShortestPaths(numS, distS, S);
+    countShortestPaths(numT, distT, T);
 
-        if (distS[e.v] == LLONG_MAX) {
-            for (Edge f : adj[e.v]) 
-                if (distS[f.u] == LLONG_MAX || distS[f.v] == LLONG_MAX) qS.push(f);
-            distS[e.v] = distS[e.u] + e.d;
-        }
-        else if (distS[e.u] == LLONG_MAX) {
-            for (Edge f : adj[e.u]) 
-                if (distS[f.u] == LLONG_MAX || distS[f.v] == LLONG_MAX) qS.push(f);
-            distS[e.u] = distS[e.v] + e.d;
-        }
-    }
-
+    LL answer = numS[T]*numS[T];
+    answer %= MOD;
     for (int i = 1; i <= N; i++) {
-        printf("%lld ", distS[i]);
+        if (distS[i] == distT[i] && 2*distS[i] == D) {
+            answer -= numS[i]*numS[i]*numT[i]*numT[i];
+            if (answer < 0) answer += MOD * (-answer/MOD);
+            while (answer < 0) answer += MOD;
+            answer %= MOD;
+        }
+        for (Edge e : adj[i]) {
+            if (2*distS[i] < D && 2*distT[e.to] < D && distS[i] + e.d + distT[e.to] == D) {
+                answer -= numS[i]*numS[i]*numT[e.to]*numT[e.to];
+                if (answer < 0) answer += MOD * (-answer/MOD);
+                while (answer < 0) answer += MOD;
+                answer %= MOD;
+            }
+        }
     }
-    printf("\n");
+    printf("%lld\n", answer);
 
     return 0;
 }
